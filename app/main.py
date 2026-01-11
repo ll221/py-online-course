4dece82e0e552a9b72c60fd931ba50684d748c98
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2026-01-11 22:35:33 +0200

    first

--- app/main.py
+++ app/main.py
@@ -1,3 +1,16 @@
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name, description, weeks):
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(days):
+        weeks = (days + 6) // 7
+        return weeks
+
+    @classmethod
+    def from_dict(cls, course_dict):
+        w = OnlineCourse()
+
+
