852b60c728dc154380f83e49520c39a55ca3cfc3
Ihor843 <ihor.kunakh.kb.2022@lpnu.ua>
2026-03-07 00:10:32 +0200

    final

--- app/main.py
+++ app/main.py
@@ -1,18 +1,21 @@
+from __future__ import annotations
+
+
 class OnlineCourse:
     def __init__(self, name: str, description: str, weeks: int) -> None:
-        self.name = name
-        self.description = description
-        self.weeks = weeks
+        self.name: str = name
+        self.description: str = description
+        self.weeks: int = weeks
 
     @staticmethod
     def days_to_weeks(days: int) -> int:
         return (days + 6) // 7
 
     @classmethod
-    def from_dict(cls, course_dict: dict) -> "OnlineCourse":
-        name = course_dict["name"]
-        description = course_dict["description"]
-        days = course_dict["days"]
-        weeks = cls.days_to_weeks(days)
+    def from_dict(cls, course_dict: dict[str, int | str]) -> OnlineCourse:
+        name: str = course_dict["name"]  # type: ignore[assignment]
+        description: str = course_dict["description"]  # type: ignore[assignment]
+        days: int = course_dict["days"]  # type: ignore[assignment]
+        weeks: int = cls.days_to_weeks(days)  # type: ignore[arg-type]
 
         return cls(name=name, description=description, weeks=weeks)
